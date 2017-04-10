3ceb2b0897d325f079713b494aabc45011c252fb
hard456 <janpalcut@seznam.cz>
2017-04-10 02:55:46 +0200

    excel team js

--- src/main/webapp/Public/js/excel_team.js
+++ src/main/webapp/Public/js/excel_team.js
@@ -1,23 +1,50 @@
-function importTeams(raceId){
-    var token = $("meta[name='_csrf']").attr("content");
-    var header = $("meta[name='_csrf_header']").attr("content");
+function importTeams(raceId, size) {
+    if (document.getElementById("my-file-selector").files.length == 0) {
+        console.log("no files selected");
+    }
+    else {
+        var token = $("meta[name='_csrf']").attr("content");
+        var header = $("meta[name='_csrf_header']").attr("content");
 
-    $(document).ajaxSend(function(e, xhr, options) {
-        xhr.setRequestHeader(header, token);
-    });
+        var url;
+        if(size == 1){
+            url = "/race/" + raceId + "/importOnePersonTeam"
+        }
+        else{
+            url = "/race/" + raceId + "/importTeams";
+        }
 
-    var formData = new FormData();
-    formData.append('file', $('input[type=file]')[0].files[0]);
+        $(document).ajaxSend(function (e, xhr, options) {
+            xhr.setRequestHeader(header, token);
+        });
 
-    $.ajax({
-        type: "POST",
-        url: "/race/" + raceId + "/importTeams",
-        data: formData,
-        contentType: false,
-        processData: false,
-        success: function (response) {
-            //console.log(response);
+        var formData = new FormData();
+        if ($('input[type=file]')[0].files[0].size > 1000000) {
+            data = '<div class="alert alert-danger">Error: file size > 1MB</div>';
+            $('#importResult').html(data);
             $('#importTeams').modal('hide');
         }
-    });
+        else {
+            formData.append('file', $('input[type=file]')[0].files[0]);
+
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: formData,
+                contentType: false,
+                processData: false,
+                success: function (response) {
+                    console.log(response);
+                    if (response.localeCompare("ok") == 0) {
+                        data = '<div class="alert alert-success">Excel file successfully imported.</div>';
+                    }
+                    else {
+                        data = '<div class="alert alert-danger">Error: ' + response + '</div>';
+                    }
+                    $('#importResult').html(data);
+                    $('#importTeams').modal('hide');
+                }
+            });
+        }
+    }
 }
