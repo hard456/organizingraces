a863302e06e71d6f6e41b86505f2683aa55e1d77
hard456 <janpalcut@seznam.cz>
2017-04-15 03:00:43 +0200

    datetime form

---
+++ src/main/java/cz/zcu/fav/sportevents/form/DatetimeTeamForm.java
@@ -0,0 +1,24 @@
+package cz.zcu.fav.sportevents.form;
+
+public class DatetimeTeamForm {
+
+    String dateTime;
+
+    Integer teamId;
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public Integer getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(Integer team) {
+        this.teamId = team;
+    }
+}
